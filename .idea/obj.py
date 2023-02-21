537c14475daa68bb565d1eae5cd8486e741d4d45
Gadeil <115675317+Gadeil@users.noreply.github.com>
2023-02-20 19:02:51 -0600

    imprimir acciones del personaje

---
+++ .idea/obj.py
@@ -0,0 +1,10 @@
+#1. importar la clase 
+from personaje import *
+
+#2. Instanciar un objeto
+heroe=personaje()
+
+#3. Acceder a sus atributos
+print("el personaje pertenece a la raza: "+ heroe.especie)
+print("se llama: "+ heroe.nombre)
+print("mide: "+ str(heroe.altura)+"metros")
